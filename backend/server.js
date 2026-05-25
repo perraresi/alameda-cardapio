c395ea34859c4dc0d7d32661eeb6f17c852eb089
Pedro <phferraresi@gmail.com>
2026-05-25 09:25:17 -0300

    remove debug logs

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,6 @@ import verificarToken from "./middleware/auth.js";
 
 dotenv.config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
 
 const app = express();
 
