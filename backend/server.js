71adde5c4b50710602953a57403cef6e7f4187a8
Pedro <phferraresi@gmail.com>
2026-05-25 09:43:25 -0300

    Corrige CORS produção

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://alameda-cardapio.vercel.app",
+    ],
   })
 );
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
